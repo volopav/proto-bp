0d0f9239f77ef5e18fc909e396c057d2a5fcfb86
Volodymy Pavlyuk <vpavlyuk@spredfast.com>
2014-11-06 16:25:01 +0200

    Added directory tree.

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,35 @@
 /**
  * Created by zuzanin on 11/6/14.
  */
+'use strict';
+
+module.exports = function(grunt) {
+
+    var LIVERELOAD_PORT = 35729;
+
+    // Load necessary grunt plugins
+    require('load-grunt-tasks')(grunt);
+
+    //Project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        //connect webserver with livereload support
+        connect: {
+            server: {
+                options: {
+                    hostname: '*',
+                    livereload: LIVERELOAD_PORT,
+                    open: 'http:localhost:8000/home.html',
+                    base: 'dist'
+                }
+            }
+        },
+
+        watch: {
+
+        }
+    });
+
+    grunt.registerTask('serve', ['connect:server', 'watch']);
+}
